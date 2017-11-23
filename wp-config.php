9fad40f3191c8a2159b0c35ed4bd87e36dd23c4d
pramod08 <spramod08@gmail.com>
2017-11-23 16:42:59 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', '192.168.1.58');
+define('DB_HOST', '104.211.191.15');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
